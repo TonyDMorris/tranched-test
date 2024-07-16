c89330d23b6c4c9a272fe01ac8a7efb3f3f475f8
Anthony Morris <anthonymorris86@googlemail.com>
2024-07-16 10:34:42 +0100

    add schema and gitignore

--- build/schema/tables.sql
+++ build/schema/tables.sql
@@ -1,55 +1,62 @@
 
 
-CREATE TABLE IF NOT EXISTS app_db.users (
-    id UUID PRIMARY KEY,
+
+CREATE TABLE IF NOT EXISTS public.users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
-CREATE INDEX ON app_db.users (username);
+CREATE INDEX ON public.users (username);
 
-CREATE TABLE IF NOT EXISTS app_db.assets (
-    id UUID PRIMARY KEY,
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS public.assets (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES public.users(id) ON DELETE CASCADE,
     symbol TEXT NOT NULL,
     amount NUMERIC NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
-INSERT INTO app_db.users (username, password_hash) VALUES (
-    "bob",
-    "$2a$10$g6efNvur33Ya9lV1Fo3ClekXylbUgv2JEl.9.21vCFRPEoBopk6k."
+INSERT INTO public.users (username, password_hash) 
+VALUES (
+    'bob',
+    '$2a$10$g6efNvur33Ya9lV1Fo3ClekXylbUgv2JEl.9.21vCFRPEoBopk6k.'
 );
 
-INSERT INTO app_db.assets (user_id, symbol, amount) VALUES (
-    (SELECT id FROM users WHERE username = "bob"),
-    "EUR",
+INSERT INTO public.assets (user_id, symbol, amount) 
+VALUES (
+    (SELECT id FROM public.users WHERE username = 'bob'),
+    'EUR',
     10000.00
 );
 
-INSERT INTO app_db.assets (user_id, symbol, amount) VALUES (
-    (SELECT id FROM users WHERE username = "bob"),
-    "USD",
+INSERT INTO public.assets (user_id, symbol, amount) 
+VALUES (
+    (SELECT id FROM public.users WHERE username = 'bob'),
+    'USD',
     10000.00
 );
 
-INSERT INTO app_db.users (username, password_hash) VALUES (
-    "tracy",
-    "$2a$10$rK2Ia/SRslB0c7GTTsHKqewhFoccS/Q/189UHCkiT6HIMKg.lHgHi"
+INSERT INTO public.users (username, password_hash) 
+VALUES (
+    'tracy',
+    '$2a$10$rK2Ia/SRslB0c7GTTsHKqewhFoccS/Q/189UHCkiT6HIMKg.lHgHi'
 );
 
-INSERT INTO app_db.assets (user_id, symbol, amount) VALUES (
-    (SELECT id FROM users WHERE username = "tracy"),
-    "EUR",
+INSERT INTO public.assets (user_id, symbol, amount) 
+VALUES (
+    (SELECT id FROM public.users WHERE username = 'tracy'),
+    'EUR',
     10000.00
 );
 
-INSERT INTO app_db.assets (user_id, symbol, amount) VALUES (
-    (SELECT id FROM users WHERE username = "tracy"),
-    "USD",
+INSERT INTO public.assets (user_id, symbol, amount) 
+VALUES (
+    (SELECT id FROM public.users WHERE username = 'tracy'),
+    'USD',
     10000.00
 );
 
